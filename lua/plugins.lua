c51076ce19081ea762d273f3df9e29dd47215306
sDaigo <s8778@g.maizuru-ct.ac.jp>
2023-12-17 23:56:57 +0900

    add nvim

---
+++ lua/plugins.lua
@@ -0,0 +1,19 @@
+return {
+  { -- colorscheme
+    'dracula/vim',
+    config = function()
+      vim.cmd([[colorscheme dracula]])
+    end
+  },
+  { -- fuzzy finder
+    'ibhagwan/fzf-lua',
+    config = function()
+      requires =  {'kyazdani42/nvim-wab-devicons'}
+    end
+  },
+  {
+    'Shougo/defx.nvim',
+    config = function()
+    end
+}
+}
