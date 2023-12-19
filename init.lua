9920bacd36240f55546c73c5aedb069373c7e0e6
sDaigo <s8778@g.maizuru-ct.ac.jp>
2023-12-19 20:55:23 +0900

    fixed

--- init.lua
+++ init.lua
@@ -5,25 +5,6 @@ require("options")
 require("keymaps")
 require("plugins")
 
-
-------------------------------------------------------------------------
---プラグイン管理lazy.nvimの設定
-------------------------------------------------------------------------
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-plugins = require('plugins')
-require ('lazy').setup(plugins)
-
-------------------------------------------------------------------------
---
+--プラグインの設定ファイル読み込み
+require("lazyvim")
+require("telescope")
